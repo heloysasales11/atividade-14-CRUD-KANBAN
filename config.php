fdc0b185a60d5d87f1a10f19dcc0308f74a592eb
Heloysa Sales <heloysa_sales@estudante.sesisenai.org.br>
2025-11-03 11:37:09 -0300

    add - config.php

---
+++ config.php
@@ -0,0 +1,34 @@
+<?php
+// config.php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit(0);
+}
+
+class Database {
+    private $host = 'localhost';
+    private $db_name = 'kanban_industria';
+    private $username = 'root';
+    private $password = '';
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
+
+function gerarToken() {
+    return bin2hex(random_bytes(32));
+}
+?>
